95f81d0e09b3fd312364424691275e46c5484ca0
Juan Manuel Flecha <flechajm@gmail.com>
2022-02-17 02:28:37 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 import Sokoban from './game/sokoban.js';
-import { levels } from './game/constants.js';
+import levels from './game/levels.js';
 
 const sokoban = new Sokoban(levels);
 sokoban.load();
 
-window.sokoban = sokoban;
+document.onreadystatechange = function () {
+    var state = document.readyState;
+    if (state == 'complete') {
+        document.getElementById('loader').style.visibility = "hidden";
+    }
+}
